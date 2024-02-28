dc95c5dd553701888325da74c633d44d72a1f0fc
Marwan Fouz <92979905+Marwanfouz@users.noreply.github.com>
2024-02-28 16:05:25 +0200

    Create 35. Search Insert Position.js [Easy]

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length;
+
+    while(left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if(target > nums[mid]) left = mid +1;
+        else right = mid;
+    }
+
+    return left;
+};
